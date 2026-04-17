eb1502bad57d1f1e1d76e40b4b5265cdd5bfcd47
moonjm <mym1207@naver.com>
2026-04-17 22:37:45 +0900

    Initial Commit

---
+++ nunting/nuntingApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuntingApp.swift
+//  nunting
+//
+//  Created by youngmin Moon on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct nuntingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
